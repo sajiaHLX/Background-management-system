3e88bd79a7e998a0ac467ba695e7e12f890d2390
sajiaHLX <690906573@qq.com>
2020-02-04 21:38:30 +0800

    完成数据报表

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from 'components/goods/Params'
 import List from 'components/goods/List'
 import Add from 'components/goods/Add'
 import Order from 'components/order/Order'
+import Report from 'components/report/Report'
 
 Vue.use(VueRouter)
 
@@ -74,6 +75,11 @@ const routes = [
         path: '/orders',
         name: 'order',
         component: Order
+      },
+      {
+        path: '/reports',
+        name: 'report',
+        component: Report
       }
     ]
   },
